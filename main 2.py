241bdf3675c6b9bd0bcbaee9bd28165548ee6980
SFantino <samantha.fantino@orange.fr>
2025-04-01 22:54:09 +0200

    Update main 2.py

--- main 2.py
+++ main 2.py
@@ -8,7 +8,7 @@ st.markdown("""
     <style>
         /* Style pour le fond d'écran */
         .stApp {
-            background: #F3F3F1 url('https://images.unsplash.com/photo-1514995669114-6081e934b693?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat left top / 40% auto;
+            background: #F3F3F1 url('https://images.unsplash.com/photo-1514995669114-6081e934b693?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat left top / 60% auto;
             min-height: 100vh;
         }
         /* Pour décaler le contenu sous l'image */
