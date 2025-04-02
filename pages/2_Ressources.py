bfbb8ef2c61378feb98bf44bb08977796185a8d6
SFantino <samantha.fantino@orange.fr>
2025-04-02 22:13:37 +0200

    Update and rename ressources.py to pages/2_Ressources.py

---
+++ pages/2_Ressources.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(page_title="Ressources | EcoImpact", layout="wide")
+
+st.title("📚 Ressources Utiles")
+st.write("""
+### Documents de référence:
+- [Guide ADEME](https://www.ademe.fr)
+- [Rapport IPCC](https://www.ipcc.ch)
+
+### Outils complémentaires:
+- [Calculateur Carbone](https://nosgestesclimat.fr)
+""")
+
+st.image("https://via.placeholder.com/800x400?text=Ressources+Visuelles", width=800)
--- ressources.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Ressources")
-st.write("Cette page contient des ressources utiles pour notre projet.")
