132247e5bb89a5fca6bccbf5da7d517071119448
SFantino <samantha.fantino@orange.fr>
2025-04-30 10:40:44 +0200

    Update main 2.py

--- main 2.py
+++ main 2.py
@@ -76,7 +76,7 @@ st.markdown("""
             right: 50%;
             margin-left: -50vw;
             margin-right: -50vw;
-            margin-top: -12px;
+            margin-top: -10px;
         }
 
         .calculator-title {
