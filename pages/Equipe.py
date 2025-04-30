c0c7e6479fc6a62ac44a5b5245684b0b75e7023b
SFantino <samantha.fantino@orange.fr>
2025-04-30 10:47:09 +0200

    Update Equipe.py

--- pages/Equipe.py
+++ pages/Equipe.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+# Configuration de la page pour l'équipe
+st.set_page_config(
+    page_title="Notre Équipe",
+    layout="wide",
+    page_icon="👥",
+)
+
+
+# ========== CONTENU PRINCIPAL DE LA PAGE "EQUIPE" ==========
+st.markdown('<div class="team-header">Notre Équipe</div>', unsafe_allow_html=True)
