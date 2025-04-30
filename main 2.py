f57521d13b35b3163cf2b348f13aa81f43ee29a4
SFantino <samantha.fantino@orange.fr>
2025-04-30 10:41:22 +0200

    Update main 2.py

--- main 2.py
+++ main 2.py
@@ -44,6 +44,7 @@ st.markdown("""
             margin-top: 70px !important;
             background: #F3F3F1 url('https://images.unsplash.com/photo-1514995669114-6081e934b693?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat left top / 65% auto;
             min-height: calc(100vh - 70px);
+            position : fixed;
         }
         
         /* Cache l'ancienne navbar */
