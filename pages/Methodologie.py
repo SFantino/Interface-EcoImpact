c39499d19c891e5a775a1d907243e81e3f760f3c
SFantino <samantha.fantino@orange.fr>
2025-04-30 09:25:23 +0200

    Update Methodologie.py

--- pages/Methodologie.py
+++ pages/Methodologie.py
@@ -85,7 +85,7 @@ def create_footer():
     st.markdown("""
         <div class="footer-banner">
             <a href="/A_propos" target="_self">À propos</a>
-            <img src="unilasalle_beauvais_logo.jpg" alt="Logo UniLaSalle Beauvais">
+            <img src="https://prepeersstorage.blob.core.windows.net/academic/1_400_logo_9f8797ed-c537-418c-9215-a420e600a540.png?sp=r&st=2025-01-30T23:00:00Z&se=2026-01-31T04:06:31Z&spr=https&sv=2022-11-02&sr=c&sig=Mm5p4fZa8%2F4%2BFA04dmK5p259BIm5Y9rzEDR8GPPJTWY%3D" alt="Logo UniLaSalle Beauvais">
         </div>
     """, unsafe_allow_html=True)
 
