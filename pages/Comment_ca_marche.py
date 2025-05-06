d122e0253587378002905d5271b35e00cab6af54
SFantino <samantha.fantino@orange.fr>
2025-05-06 17:54:14 +0200

    Update and rename Methodologie.py to Comment_ca_marche.py

--- pages/Comment_ca_marche.py
+++ pages/Comment_ca_marche.py
@@ -4,7 +4,7 @@ from design import load_css, create_navbar, create_footer
 
 # ========== CONFIGURATION ==========
 st.set_page_config(
-    page_title="Méthodologie | EcoImpact",
+    page_title="Comment ça marche ? | EcoImpact",
     layout="wide",
     page_icon="📊",
     initial_sidebar_state="collapsed"
@@ -14,7 +14,7 @@ st.set_page_config(
 def methodo_content():
     st.markdown('<div class="content-behind">', unsafe_allow_html=True)
     
-    st.markdown("<h1 style='color:#000000;'>📊 Méthodologie Scientifique</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color:#000000;'>📊 Comment ça marche ?</h1>", unsafe_allow_html=True)
     
     st.markdown('</div>', unsafe_allow_html=True)
 
