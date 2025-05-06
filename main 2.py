281663f40e88acb10a99cccf621d063b2d010e08
SFantino <samantha.fantino@orange.fr>
2025-05-06 17:00:31 +0200

    Update main 2.py

--- main 2.py
+++ main 2.py
@@ -47,7 +47,7 @@ st.markdown("""
             padding: 25px;
             border-radius: 0;
             width: 100vw;
-            margin: 300px 0 20px 0;
+            margin: 400px 0 20px 0;
             text-align: center;
             box-shadow: 0 4px 8px rgba(0,0,0,0.1);
             position: fixed;
