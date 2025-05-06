37248a945811473fd1e15103014cac8e9973280a
SFantino <samantha.fantino@orange.fr>
2025-05-06 17:55:27 +0200

    Update and rename Ressources.py to Pourquoi_ce_projet.py

--- pages/Pourquoi_ce_projet.py
+++ pages/Pourquoi_ce_projet.py
@@ -3,9 +3,9 @@ from design import load_css, create_navbar, create_footer
 
 # ========== CONFIGURATION ==========
 st.set_page_config(
-    page_title="Ressources | EcoImpact",
+    page_title="Pourquoi ce projet ? | EcoImpact",
     layout="wide",
-    page_icon="📚",
+    page_icon="🌍",
     initial_sidebar_state="collapsed"
 )
 
@@ -14,7 +14,7 @@ st.set_page_config(
 def ressources_content():
     st.markdown('<div class="content-behind">', unsafe_allow_html=True)
     
-    st.markdown("<h1 style='color:#000000;'>📚 Ressources Utiles</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color:#000000;'>🌍 Pourquoi ce projet ?", unsafe_allow_html=True)
     
 
     st.markdown('</div>', unsafe_allow_html=True)
