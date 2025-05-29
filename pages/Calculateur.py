637fa54586ef73051bba5c24212f7fe378f0d74b
SFantino <samantha.fantino@orange.fr>
2025-05-29 19:53:51 +0200

    Update Calculateur.py

--- pages/Calculateur.py
+++ pages/Calculateur.py
@@ -5,19 +5,31 @@ from calculateur.Variables import variables
 from calculateur.etapes_panier import etapes_panier
 from design import load_css, create_navbar, create_footer
 
-with st.container():
-    st.markdown("""
-        <div style="background-color: white; padding: 2rem; min-height: 100vh;">
+st.markdown("""
+    <style>
+    .stApp {
+        background-color: white !important;
+    }
+    </style>
     """, unsafe_allow_html=True)
 
-    # Ton code principal ici
-    gerer_panier()
-    codes_ciqual = [int(produit["code_ciqual"]) for produit in st.session_state.panier]
-    score_panier()
-    variables()
-    etapes_panier()
 
-    st.markdown("</div>", unsafe_allow_html=True)
+# ========== CONTENU PRINCIPAL ==========
+# Appeler la fonction qui gère tout le panier
+gerer_panier()
+
+# Affichage des codes CIQUAL dans le panier
+codes_ciqual = [int(produit["code_ciqual"]) for produit in st.session_state.panier]
+
+
+# Appeler la fonction pour calculer le score du panier
+score_panier()
+
+# Appeler la fonction pour afficher les variables environnementales
+variables()
+
+# Appeler la fonction pour afficher les comparaison des étapes pour le panier
+etapes_panier()
 
 
 
