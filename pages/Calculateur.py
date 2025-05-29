43d232a1b7cc1a2d96055598ec11ec66a8ff4a1e
SFantino <samantha.fantino@orange.fr>
2025-05-29 19:52:08 +0200

    Update Calculateur.py

--- pages/Calculateur.py
+++ pages/Calculateur.py
@@ -5,38 +5,23 @@ from calculateur.Variables import variables
 from calculateur.etapes_panier import etapes_panier
 from design import load_css, create_navbar, create_footer
 
+with st.container():
+    st.markdown("""
+        <div style="background-color: white; padding: 2rem; min-height: 100vh;">
+    """, unsafe_allow_html=True)
 
+    # Ton code principal ici
+    gerer_panier()
+    codes_ciqual = [int(produit["code_ciqual"]) for produit in st.session_state.panier]
+    score_panier()
+    variables()
+    etapes_panier()
 
-# ========== CONTENU PRINCIPAL ==========
-# Appeler la fonction qui gère tout le panier
-gerer_panier()
-
-# Affichage des codes CIQUAL dans le panier
-codes_ciqual = [int(produit["code_ciqual"]) for produit in st.session_state.panier]
-
-
-# Appeler la fonction pour calculer le score du panier
-score_panier()
-
-# Appeler la fonction pour afficher les variables environnementales
-variables()
-
-# Appeler la fonction pour afficher les comparaison des étapes pour le panier
-etapes_panier()
+    st.markdown("</div>", unsafe_allow_html=True)
 
 
 
 # ========== STRUCTURE ==========
 load_css()
-
-st.markdown("""
-    <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        document.body.style.background = "white";
-        document.querySelector(".stApp").style.background = "white";
-    });
-    </script>
-    """, unsafe_allow_html=True)
-
 create_navbar()
 create_footer()
