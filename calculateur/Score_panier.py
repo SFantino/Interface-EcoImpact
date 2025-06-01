0435cce68b31da3d3f8480f05c22706c4207d97b
SFantino <samantha.fantino@orange.fr>
2025-06-01 22:12:49 +0200

    Update Score_panier.py

--- calculateur/Score_panier.py
+++ calculateur/Score_panier.py
@@ -72,8 +72,9 @@ def score_panier():
         segments = []
         valeur_relative = (score_moyen - score_min) / borne_totale
         
+       valeur_relative = (score_moyen - score_min) / (score_max - score_min)
+
         segments = []
-        
         for i, (classe, couleur) in enumerate(classes):
             debut = i / len(classes)
             fin = (i + 1) / len(classes)
@@ -87,21 +88,22 @@ def score_panier():
             else:
                 segments.append(f'<div style="flex:1; background:#eee; height:100%;"></div>')
         
-            curseur_position = f"{valeur_relative * 100:.2f}%"
-            curseur_html = f'''
-            <div style="position: absolute; left: {curseur_position}; top: 0; bottom: 0; width: 2px; background: black;"></div>
-            '''
-            
-            bar_html = f'''
-            <div style="position: relative; height: 30px; border-radius: 5px; display: flex; overflow: hidden;">
-                {''.join(segments)}
-                {curseur_html}
-                <span style="position: absolute; left: 0; top: 5px; font-weight: bold;">{score_min:.2f}</span>
-                <span style="position: absolute; right: 0; top: 5px; font-weight: bold;">{score_max:.2f}</span>
-            </div>
-            '''
-            
-            st.markdown(bar_html, unsafe_allow_html=True)
+        curseur_position = f"{valeur_relative * 100:.2f}%"
+        curseur_html = f'''
+        <div style="position: absolute; left: {curseur_position}; top: 0; bottom: 0; width: 2px; background: black;"></div>
+        '''
+        
+        bar_html = f'''
+        <div style="position: relative; height: 30px; border-radius: 5px; display: flex; overflow: hidden;">
+            {''.join(segments)}
+            {curseur_html}
+            <span style="position: absolute; left: 0; top: 5px; font-weight: bold;">{score_min:.2f}</span>
+            <span style="position: absolute; right: 0; top: 5px; font-weight: bold;">{score_max:.2f}</span>
+        </div>
+        '''
+        
+        st.markdown(bar_html, unsafe_allow_html=True)
+
 
 
         
